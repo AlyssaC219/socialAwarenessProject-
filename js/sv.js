63fb55a5a602c00ea9d3c33a441ddc16016f822d
Sash149 <90012406+Sash149@users.noreply.github.com>
2021-12-12 16:51:07 -0500

    Add pages

---
+++ js/sv.js
@@ -0,0 +1,33 @@
+let pictures = ["social-awareness-picture-books.png", "2ndpicture.png"];
+
+let descriptions = [
+  "12312312, This is a description, This is a description, ",
+  "2nd description"
+];
+
+let nextPage = "#";
+
+let rightSection = document.querySelector("#rightSection");
+for (let i = 0; i < pictures.length; i++) {
+  let list = document.createElement("div");
+  let left = document.createElement("div");
+  let right = document.createElement("div");
+  list.appendChild(left);
+  list.appendChild(right);
+  list.setAttribute("class", "list-wrapper");
+  left.setAttribute("class", "image");
+  right.setAttribute("class", "description");
+  let imageTag = document.createElement("img");
+  imageTag.setAttribute("width", "50px"); //
+  imageTag.setAttribute("src", "./" + pictures[i]);
+  left.appendChild(imageTag);
+  right.innerText = descriptions[i];
+
+  rightSection.appendChild(list);
+}
+
+let button = document.querySelector("#btn");
+button.addEventListener("click", (event) => {
+  console.log("123");
+  location.href = nextPage;
+});
